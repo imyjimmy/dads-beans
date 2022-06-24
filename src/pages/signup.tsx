31959e05f7cab206a5f7989fa785b59817a036c5
Jimmy Zhang <imyjimmy@gmail.com>
2022-06-24 16:41:51 -0500

    temp comment out 'or continue with' section

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -154,7 +154,7 @@ const SignUp: React.FC<Props> = (): ReactElement => {
               </div>
             </form>
 
-            <div className='mt-6'>
+            {/* <div className='mt-6'>
               <div className='relative'>
                 <div className='absolute inset-0 flex items-center'>
                   <div className='w-full border-t border-gray-300' />
@@ -226,7 +226,7 @@ const SignUp: React.FC<Props> = (): ReactElement => {
                   </a>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
