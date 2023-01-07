ad959a35738605de3f45e2fb2f9baba3226ace22
Jimmy Zhang <imyjimmy@gmail.com>
2023-01-07 15:29:18 -0800

    free shipping over 50 usd, otherwise .50

--- src/pages/api/checkout_sessions/cart.ts
+++ src/pages/api/checkout_sessions/cart.ts
@@ -21,6 +21,11 @@ const query = gql`
   }
 `
 
+type Item = {
+  price_data: any
+  quantity: any
+}
+
 /*
  * Product data can be loaded from anywhere. In this case, we’re loading it from
  * a local JSON file, but this could also come from an async call to your
@@ -61,19 +66,52 @@ export default async function handler(
       //   return !!item.price_data.recurring
       // })
 
+      const total = line_items
+        .map((item: Item) => item.price_data.unit_amount * item.quantity)
+        .reduce((prev: number, price: number) => prev + price)
+
+      console.log('total: ', total)
+
+      let shippingRates
+      if (total > 5000) {
+        shippingRates = {}
+      } else {
+        shippingRates = await fetch(
+          `https://api.stripe.com/v1/shipping_rates/
+          ${process!.env!.STRIPE_SHIPPING_RATE}`,
+          {
+            headers: {
+              Authorization: `Bearer ${process!.env!.STRIPE_SECRET_KEY}`,
+            },
+          }
+        ).then((response) => response.json())
+      }
+
       // Create Checkout Sessions from body params.
       const params: Stripe.Checkout.SessionCreateParams = {
         submit_type: 'pay',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_address_collection: {
-          allowed_countries: ['US', 'CA'],
+          allowed_countries: ['US'],
         },
+        shipping_options: [
+          shippingRates
+            ? {
+                shipping_rate_data: {
+                  display_name: shippingRates.display_name,
+                  fixed_amount: shippingRates.fixed_amount,
+                  type: shippingRates.type,
+                },
+              }
+            : {},
+        ],
         line_items,
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/use-shopping-cart`,
         mode: 'payment', // hasSubscription ? 'subscription' :
       }
+      console.log('line items:', line_items)
 
       const checkoutSession: Stripe.Checkout.Session =
         await stripe.checkout.sessions.create(params)
